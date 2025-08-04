a3fa51d919130fc0abde7ec0d4a3881e91d43e75
Hassan Sadiq <148046818+TSGCFO@users.noreply.github.com>
2025-08-04 13:48:54 +0000

    Export

--- list-secrets.js
+++ list-secrets.js
@@ -4,11 +4,6 @@
 
 import fs from 'fs';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-// Get current directory (needed in ES modules)
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 console.log("=== Exporting Specific Environment Variables to .env File ===");
 
